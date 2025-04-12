f199539902388e1572a0569c23370688e22d800d
苏向夜 <fu050409@163.com>
2025-04-12 22:15:58 +0800

    chore: setup postcss and uv

--- farm.config.ts
+++ farm.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from '@farmfe/core';
+
+import postcss from '@farmfe/js-plugin-postcss';
 import vue from 'unplugin-vue/vite';
 
 export default defineConfig({
+    plugins: [postcss()],
     vitePlugins: [vue()],
 });
