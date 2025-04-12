0d739716aad599219add0b1b73d344d499f6f5cc
苏向夜 <fu050409@163.com>
2025-04-12 22:36:12 +0800

    chore: fix unplugin compatibility

--- farm.config.ts
+++ farm.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@farmfe/core';
 
 import postcss from '@farmfe/js-plugin-postcss';
-import vue from 'unplugin-vue/vite';
+import vue from 'unplugin-vue/rollup';
 
 export default defineConfig({
     plugins: [postcss()],
